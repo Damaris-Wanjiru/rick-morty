38ca452e8fad952405f0e7890299c28459769f81
Damaris-Wanjiru <damarshih@gmail.com>
2024-04-02 08:45:48 +0300

    rick and morty explorer using html and javascript

---
+++ morty.js
@@ -0,0 +1,11 @@
+function redirectTo(page) {
+    window.location.href {page.html}
+}
+
+function redirectTo(location) {
+    window.location.href {location.html}
+}
+
+function redirectTo(episodes) {
+    window.location.href = $ {episodes.html}
+}
